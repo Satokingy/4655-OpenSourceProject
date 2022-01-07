197812390b54c2ea1d4a0206189648246d16a4c3
Satokingy <sumahootakujp@gmail.com>
2022-01-08 00:14:18 +0900

    Create Goroku.cs

---
+++ Goroku.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goroku : MonoBehaviour
+{
+    public void Dowa()	//どうなるか、わかってるのかな？
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1435027927133876225");
+    }
+    public void Yamete()	以前、もうやめてと
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1461018776615337986");
+    }
+    public void Chotto()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1468348163480166404");
+    }
+    public void Chotto2()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1469899004289110019");
+    }
+    public void Dinner()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1410899041748086786");
+    }
+    public void Filter()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1467832904277688320");
+    }
+    public void Hair()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1425329550234296326");
+    }
+    public void Hot()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1419200301626576897");
+    }
+    public void Ice()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1411198934475624452");
+    }
+    public void Ima()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1461108041663799296");
+    }
+    public void Iroiro()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1410541615354052614");
+    }
+    public void KanSuuzi()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1469714461351378944");
+    }
+    public void Kotoba()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1410282756970475520");
+    }
+    public void Lunch()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1415513016817258500");
+    }
+    public void Minna()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1469836005339725827");
+    }
+    public void Nino()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1476798461844795392");
+    }
+    public void OO()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1469708188450967553");
+    }
+    public void Rain()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1410625411369771012");
+    }
+    public void Second()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1467989677974392834");
+    }
+    public void Second2()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1468245672814120967");
+    }
+    public void Semete()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1467871465232281601");
+    }
+    public void Sleep()	//寝ようかな〜
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1412059233772658688");
+    }
+    public void Sleep2() //寝すぎ。起きろ。
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1414045633191682051");
+    }
+    public void StudyTest()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1467767408702427137");
+    }
+    public void Tanin()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1461217262547857409");
+    }
+    public void Tiame()	//時間を決めて貸していたのに、途中から「取った」とか言われるようになった。
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1469699934845026306");
+    }
+    public void Umai()
+    {
+        Application.OpenURL("https://twitter.com/kn4655/status/1412651002189160449");
+    }
+}
